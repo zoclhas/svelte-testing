a9f163b11a0f4c0a72d8c129aae33a8ad6f4a2f4
Zoclhas <zoclhas@gmail.com>
2023-10-22 13:34:57 +0400

    prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script>
+	let i = 0;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="text-xl">f</div>
+<h1 class="text-4xl">
+	{i}
+</h1>
+
+<button on:click={() => (i += 1)}>+</button>
+<button on:click={() => (i -= 1)}>-</button>
